e8f4b37c6b9451152c6104c46e374aa2aca48a20
OttavioM <ottavio.mazzaretto@gmail.com>
2024-02-14 20:38:52 +0100

    js\day-night-js.js- added the white <p> when night and black when day

--- js/day-night-js.js
+++ js/day-night-js.js
@@ -8,11 +8,13 @@ document.addEventListener('DOMContentLoaded', function () {
     // Loop through elements with class 'day-night' and toggle their background colors
     elementsToToggle.forEach(function (element) {
       element.style.backgroundColor = this.checked ? 'white' : 'black';
+      element.style.color = this.checked ? 'black' : 'white';
     }, this);
   });
 
   // Set the initial background color for elements with class 'day-night' based on the checkbox state
   elementsToToggle.forEach(function (element) {
     element.style.backgroundColor = toggleCheckbox.checked ? 'white' : 'black';
+    element.style.color = toggleCheckbox.checked ? 'black' : 'white';
   });
 });
