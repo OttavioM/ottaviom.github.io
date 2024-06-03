c1d82959bb2d34e1afbdee441847f49f7728bfe7
OttavioM <ottavio.mazzaretto@gmail.com>
2024-06-03 13:03:37 +0200

    js\day-night-js.js - update to show in console the error

--- js/day-night-js.js
+++ js/day-night-js.js
@@ -3,18 +3,22 @@ document.addEventListener('DOMContentLoaded', function () {
   const toggleCheckbox = document.querySelector('.toggle--checkbox');
   const elementsToToggle = document.querySelectorAll('.day-night');
 
-  // Add an event listener to the checkbox
-  toggleCheckbox.addEventListener('change', function () {
-    // Loop through elements with class 'day-night' and toggle their background colors
-    elementsToToggle.forEach(function (element) {
-      element.style.backgroundColor = this.checked ? 'white' : 'black';
-      element.style.color = this.checked ? 'black' : 'white';
-    }, this);
-  });
+  if (toggleCheckbox && elementsToToggle.length > 0) {
+    // Add an event listener to the checkbox
+    toggleCheckbox.addEventListener('change', function () {
+      // Loop through elements with class 'day-night' and toggle their background colors
+      elementsToToggle.forEach(function (element) {
+        element.style.backgroundColor = this.checked ? 'white' : 'black';
+        element.style.color = this.checked ? 'black' : 'white';
+      }, this);
+    });
 
-  // Set the initial background color for elements with class 'day-night' based on the checkbox state
-  elementsToToggle.forEach(function (element) {
-    element.style.backgroundColor = toggleCheckbox.checked ? 'white' : 'black';
-    element.style.color = toggleCheckbox.checked ? 'black' : 'white';
-  });
+    // Set the initial background color for elements with class 'day-night' based on the checkbox state
+    elementsToToggle.forEach(function (element) {
+      element.style.backgroundColor = toggleCheckbox.checked ? 'white' : 'black';
+      element.style.color = toggleCheckbox.checked ? 'black' : 'white';
+    });
+  } else {
+    console.error('Toggle checkbox or elements to toggle not found.');
+  }
 });
