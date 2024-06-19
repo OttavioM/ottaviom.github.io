15dac3a6d9cb6d7e80d1777d1b17a351181feb4c
OttavioM <ottavio.mazzaretto@gmail.com>
2024-06-19 09:58:03 +0200

    js\swwsys-wsys-btn.js - working for the initial plots

--- js/swwsys-wsys-btn.js
+++ js/swwsys-wsys-btn.js
@@ -14,11 +14,33 @@ function handleButtonClick(buttonId) {
         // Update button styles
         swwsysButton.classList.add('btn-active_sys');
         wsysButton.classList.remove('btn-active_sys');
+        
+        // Update the script content
+        updateJsonPaths('/jsons_plotly/sum_99/scatter.json');
     } else if (buttonId === 'WSYs') {
         // Change the text content to the new text
         waveClimateText.textContent = 'WAVE CLIMATE SYSTEMS';
         // Update button styles
         swwsysButton.classList.remove('btn-active_sys');
         wsysButton.classList.add('btn-active_sys');
+        
+        // Update the script content
+        updateJsonPaths('/jsons_plotly_WS/sum_99/scatter.json');
     }
+}
+
+function updateJsonPaths(path) {
+    // Clear the card-body
+    var cardBody = document.getElementById('card-body');
+    cardBody.innerHTML = '';
+    
+    // Create a new script element
+    var script = document.createElement('script');
+    script.innerHTML = `
+        loadJSONAndStore('${path}');
+        myFunction_reset_view('${path}', {});
+    `;
+    
+    // Append the new script to the card-body
+    cardBody.appendChild(script);
 }
